7b6629a7d2a736bb6ed5c518902cbb5ce835b4e0
Anjali Pawar <anjalipawar2206@gmail.com>
2023-12-30 23:45:30 +0530

    Food Ordering App

---
+++ App.js
@@ -0,0 +1,3 @@
+const heading1=React.createElement("h1",{id:"heading"},"Hello World(React)");
+const root1=ReactDOM.createRoot(document.getElementById("root"));
+root1.render(heading1);
---
+++ README.md
@@ -0,0 +1,1 @@
+Humger Hub
